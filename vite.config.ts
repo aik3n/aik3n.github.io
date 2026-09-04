7aee780ac2e8b9441b601c748eadd05da0b6be1c
aik3n <sam.cdi110@gmail.com>
2026-09-04 13:35:54 +0200

    ruta, zenode

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   // ZeNode se publica como "project site":
   // https://aik3n.github.io/ZeNode/
-  base: '/ZeNode/',
+  base: '/',
   plugins: [svelte()]
 });
